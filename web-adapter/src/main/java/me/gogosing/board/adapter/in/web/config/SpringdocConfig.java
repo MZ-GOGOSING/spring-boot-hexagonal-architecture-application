254485f1e3ed011d7143f3a37c8fa5bb9cd74e71
정진범 <mz_gogosing@woowafriends.com>
2022-06-09 13:25:26 +0900

    springdoc 설정

---
+++ web-adapter/src/main/java/me/gogosing/board/adapter/in/web/config/SpringdocConfig.java
@@ -0,0 +1,24 @@
+package me.gogosing.board.adapter.in.web.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!prod")
+@Configuration
+@OpenAPIDefinition
+public class SpringdocConfig {
+
+	@Bean
+	public OpenAPI springBootHexagonalArchitectureOpenAPI() {
+		Info info = new Info()
+			.title("spring-boot-hexagonal-architecture-application API")
+			.description("Hexagonal Architecture Sample Application")
+			.version("v0.0.1");
+
+		return new OpenAPI().info(info);
+	}
+}
